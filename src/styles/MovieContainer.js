46f1e5b1f93a222a3c51f3053df1002083f032bf
floflix <cleidson.ferreira@gmail.com>
2021-05-09 19:04:22 -0300

    Inicializando repositorio

---
+++ src/styles/MovieContainer.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const MoviesContainer = styled.section`
+  paading: 15px;
+  border: 1px solid black;
+  border-radius: 10px;
+  background: #cecece;
+`;
